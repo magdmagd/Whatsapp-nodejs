64d0ef600bead9ddaa2433b48b2526550f7d06f2
unknown <IDISuperAdmin@IDIDomain2020.com>
2022-09-07 15:41:31 +0200

    Update App (Basic Coding)

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const createError = require('http-errors');
 const morgan = require('morgan');
 const { Client , LocalAuth } = require('whatsapp-web.js');
-const qrCode = require('qrcode');
+const qrCode = require('qrcode-terminal');
 
 //require('dotenv').config();
 
@@ -19,20 +19,70 @@ const qrCode = require('qrcode');
 const app   = express();
 const config = require("./config.json");
 const port  = process.env.port || config.port ;
+const client = new Client();
 
-
-
-
-
-
-                                                  
                                                       /*  
                                                       
                                                         Coding 
 
                                                           */
+app.use(express.json());                                                         
 app.use(bodyParser.urlencoded({extended:true}))   ;
 app.use(bodyParser.json({limit :"50mb"}));
+
+
+
+client.on('qr', (qr) => 
+{
+  qrCode.generate(qr, { small: true });
+});//Generate QR Code 
+
+
+
+client.on("authenticated",()=>
+{
+
+  console.log("Client Authinticated Successfully") ;
+
+});
+
+client.on('auth_failure', msg => 
+{
+  // Fired if session restore was unsuccessfull
+  console.error('AUTHENTICATION FAILURE', msg);
+  process.exit();
+});//check session restore was unsuccessfull
+
+
+client.on('ready', () => 
+{
+  console.log('Client is ready!');
+});//Ready Status 
+
+client.on('disconnected',()=>
+{
+  console.log('Client is disconnected!');
+});//Client was disconnected 
+
+client.on('message', message => 
+{
+	console.log(message.body);
+});//Read Message
+
+client.on('message', message => 
+{
+	if(message.body === 'ping') {
+		message.reply('pong');
+	}
+});//Read & Check Message
+
+ 
+client.initialize();
+
+app.listen(port,()=>
+{
+  console.log("Server Is Running on "+port);
+});//Create Listen Service for app 
  
 
 module.exports = app;
