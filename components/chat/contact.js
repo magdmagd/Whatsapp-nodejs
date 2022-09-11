8e17b2045e84eb52e73de7cf553e594c7ae4954e
unknown <IDISuperAdmin@IDIDomain2020.com>
2022-09-11 15:38:45 +0200

    Update (1) Contact Item

--- components/chat/contact.js
+++ components/chat/contact.js
@@ -31,7 +31,57 @@ router.get('/getcontacts', (req, res) =>
 });//Ready Status 
 
   
+router.get('/getcontact/:phone', async (req, res) => 
+{
+    let phone = req.params.phone;
 
+    if (phone != undefined) 
+    {
+        client.getContactById(`${phone}@c.us`).then((contact) => 
+        {
+            res.send(JSON.stringify(contact));
+        }).catch((err) => {
+            res.send({ status: 'error', message: 'Not found' });
+        });
+    }//end if
+});//end getContatID
+
+router.get('/getprofilepic/:phone', async (req, res) => {
+    let phone = req.params.phone;
+
+    if (phone != undefined) 
+    {
+        client.getProfilePicUrl(`${phone}@c.us`).then((imgurl) => 
+        {
+            if (imgurl) 
+            {
+                res.send({ status: 'success', message: imgurl });
+            }//end if 
+            else 
+            {
+                res.send({ status: 'error', message: 'Not Found' });
+            }//end else 
+        })//end client
+    }//end if
+});//End Get profile PIC 
+
+router.get('/isregistereduser/:phone', async (req, res) => 
+{
+    let phone = req.params.phone;
+    
+    if (phone != undefined) {
+        client.isRegisteredUser(`${phone}@c.us`).then((is) =>
+         {
+
+            is ? res.send({ status: 'success', message: `${phone} is a whatsapp user` })
+                : res.send({ status: 'error', message: `${phone} is not a whatsapp user` });
+        })
+    } 
+    else
+     {
+        res.send({ status: 'error', message: 'Invalid Phone number' });
+     }//end else 
+});
                      
 
 
